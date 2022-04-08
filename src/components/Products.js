2eef87edba948e6430d29b82cf6f419fe8ac0935
s.aaru_ <aryan.shakya760@gmail.com>
2022-04-08 08:48:05 +0545

    cart-component

--- src/components/Products.js
+++ src/components/Products.js
@@ -2,32 +2,35 @@ import React, { Component } from 'react'
 import formatCurrency from '../currency'
 
 export default class Products extends Component {
-  render() {
-    return (
-      <div>
-          <ul className='products'>
-              {this.props.products.map (product => (
-                  <li key={product.id}>
-                    <div className='products'>
-                        <a href={'#' + product.id}>
-                            <img src={product.image} alt={product.title}></img>
-                        <p>
-                            {product.description}
-                        </p>
-                        </a>
-                        <div className='product-price'>
-                            <div>
-                                {formatCurrency(product.price)}
+    render() {
+        return (
+            <div>
+                <ul className='products'>
+                    {this.props.products.map(product => (
+                        <li key={product.id}>
+                            <div className='products'>
+                                <a href={'#' + product.id}>
+                                    <img src={product.image} alt="img"></img>
+                                    <p>
+                                        {product.description}
+                                    </p>
+                                </a>
+                                <div className='product-price'>
+                                    <div>
+                                        {formatCurrency(product.price)}
+                                    </div>
+                                    <button
+                                        onClick={() => this.props.addToCart(product)}
+                                        className="button primary"
+                                    >
+                                        Add To Cart
+                                    </button>
+                                </div>
                             </div>
-                            <button className='button primary'>
-                                Add to Cart
-                            </button>
-                        </div>
-                    </div>
-                  </li>
-              ))}
-          </ul>
-      </div>
-    )
-  }
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        )
+    }
 }
